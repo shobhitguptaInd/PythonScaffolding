22bcdc13e7da315a6c0428b228fc44040fa662b9
Shobhit <shobhitguptsho-gcp@gmail.com>
2024-05-19 07:42:34 +0000

    GCP edits

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import jsonify
+app = Flask(_name_)
+
+@app.route('/')
+def hello():
+    """Return friendly HTTP greeting"""
+    print("I am inside a hello world")
+    return 'Hello world!'
+
+@app.route('/echo/<name>')
+def echo(name):
+    print(f" this was placed in the url: new-{name}")
+    val= {"new-name": name}
+    return jsonify(val)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
